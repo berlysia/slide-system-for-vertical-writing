32339bbfc8ac7f11fe72c424a00dbd53c6917610
berlysia <berlysia@gmail.com>
2025-03-07 20:46:48 +0900

    deploy slides

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import mdx from '@mdx-js/rollup'
-import slidesPlugin from './src/vite-plugin-slides'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import mdx from "@mdx-js/rollup";
+import slidesPlugin from "./src/vite-plugin-slides";
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
+  base: "./",
   resolve: {
     alias: {
-      '@components': '/src/slide-components'
-    }
+      "@components": "/src/slide-components",
+    },
   },
   plugins: [
     mdx({
       jsx: true,
-      jsxImportSource: 'react',
-      providerImportSource: '@mdx-js/react'
+      jsxImportSource: "react",
+      providerImportSource: "@mdx-js/react",
     }),
     react(),
-    await slidesPlugin()
+    await slidesPlugin({
+      slidesDir: "slides",
+      collection: process.env.SLIDE_PATH || undefined,
+    }),
   ],
   optimizeDeps: {
     // Add virtual module to optimization
-    include: ['virtual:slides.jsx']
-  }
-}))
+    include: ["virtual:slides.jsx"],
+  },
+}));
